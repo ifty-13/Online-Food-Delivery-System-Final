c52b49c035e83536457e2fce45e137517dea3a22
DipRonjon <biswas23105101587@diu.edu.bd>
2024-12-15 20:56:15 +0600

    Update food.sql
    Data base table for food

--- food.sql
+++ food.sql
@@ -205,19 +205,19 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `food`.`payment` (
-  `payment_id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(20) NOT NULL,
-  `card_type` VARCHAR(20) NOT NULL,
-  `amount_spent` DECIMAL(15,3) NOT NULL,
-  `time_stamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`payment_id`),
-  INDEX `username` (`username` ASC) VISIBLE,
-  CONSTRAINT `payment_ibfk_1`
+                                                `payment_id` INT NOT NULL AUTO_INCREMENT,
+                                                `username` VARCHAR(20) NOT NULL,
+    `card_type` VARCHAR(20) NOT NULL,
+    `amount_spent` DECIMAL(15,3) NOT NULL,
+    `time_stamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`payment_id`),
+    INDEX `username` (`username` ASC) VISIBLE,
+    CONSTRAINT `payment_ibfk_1`
     FOREIGN KEY (`username`)
     REFERENCES `food`.`details` (`username`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
 INSERT INTO `food`.`payment` (user_id, amount) VALUES (7, 1000);
 
