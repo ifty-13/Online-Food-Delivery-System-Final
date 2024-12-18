2cc14155bcaa7c4ba02911a02d5b94751630447c
DipRonjon <biswas23105101587@diu.edu.bd>
2024-12-18 21:21:13 +0600

    dbconnector  for prfile
    here is  the code for connecting the view profile with the database

--- src/ViewProfile.java
+++ src/ViewProfile.java
@@ -194,14 +194,14 @@ public class ViewProfile extends JFrame{
 
 	public void dbConnect() {
 		try {
-            Class.forName("java.sql.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
-            stmt = con.createStatement();
+			Class.forName("java.sql.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
+			stmt = con.createStatement();
 
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
 
-        }
+		}
 	}
 
 	public static void main(String[] args) {
