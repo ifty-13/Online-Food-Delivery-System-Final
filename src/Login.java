9af4c1b069c65439497bb6491a7cf53108e4535e
DipRonjon <biswas23105101587@diu.edu.bd>
2024-12-18 23:19:54 +0600

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -179,15 +179,5 @@ public class Login extends JFrame implements ActionListener{
 
     }
 
-    public void dbConnect() {
-        try {
-            Class.forName("java.sql.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
-            stmt = con.createStatement();
-
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
-
-        }
-    }
+  
 }
