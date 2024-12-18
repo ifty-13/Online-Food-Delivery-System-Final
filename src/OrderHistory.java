6d964b0593e9b7e6a28bf1a5bc222b1f33f8718d
DipRonjon <haque2305101745@diu.edu.bd>
2024-12-19 00:00:25 +0600

    Update OrderHistory.java

--- src/OrderHistory.java
+++ src/OrderHistory.java
@@ -18,27 +18,27 @@ import javax.swing.table.TableCellRenderer;
 public class OrderHistory extends JFrame{
 	private JLabel title;
 	private JButton ok;
-	
+
 	private JTable jTable1;
 	private JScrollPane jScrollPane1;
 	private DefaultTableModel model;
-	
+
 	static Connection con = null;
-    static Statement stmt = null;
-    static ResultSet rs = null;
-    
-    //int id[] = new int[20];
-    //String user[] = new String[20];
-    //int totalitems[] = new int[20];
-    //float amt[] = new float[20];
-    //String datetime[] = new String[20];
-	
+	static Statement stmt = null;
+	static ResultSet rs = null;
+
+	//int id[] = new int[20];
+	//String user[] = new String[20];
+	//int totalitems[] = new int[20];
+	//float amt[] = new float[20];
+	//String datetime[] = new String[20];
+
 	public OrderHistory() {
 		super("Order History");
-	
+
 		//Object row[] = {"Order ID", "Username", "Total Items", "Amount", "Order Status", "Time Stamp"};
-        //model.addRow(row);
-		
+		//model.addRow(row);
+
 		try{
 			initComponents();
 			DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
@@ -47,22 +47,22 @@ public class OrderHistory extends JFrame{
 			//System.out.println("From Login "+log.user);
 			PreparedStatement pst = (PreparedStatement) con.prepareStatement("SELECT * FROM food.orders WHERE username = ?");
 			pst.setString(1, log.user);
-			ResultSet rs = pst.executeQuery(); 
+			ResultSet rs = pst.executeQuery();
 
 			while(rs.next()){
 				//System.out.println("Query executed");
-				
+
 				int id = rs.getInt("order_id");
 				String user = rs.getString("username");
 				int totalitems = rs.getInt("total_items");
 				float amt = rs.getFloat("amount");
 				String orderstat = rs.getString("order_status");
 				String datetime = rs.getString("time_stamp");
-				
+
 				//System.out.println(ema + " "+ first+" "+last+" "+pho+" "+sta);
-				
+
 				Object row[] = {id, user, totalitems, amt, orderstat, datetime};
-		        model.addRow(row);
+				model.addRow(row);
 			}
 		}
 		catch(Exception except) {
@@ -134,7 +134,7 @@ public class OrderHistory extends JFrame{
 		// Set a custom renderer and editor for the "Review" column
 		jTable1.getColumn("Review").setCellRenderer(new ButtonRenderer());
 		jTable1.getColumn("Review").setCellEditor(new ButtonEditor(new JCheckBox()));
-	//	jTable1.getColumnModel().getColumn(6).setPreferredWidth(100);
+		//	jTable1.getColumnModel().getColumn(6).setPreferredWidth(100);
 
 	}
 
@@ -231,16 +231,16 @@ public class OrderHistory extends JFrame{
 
 	public void dbConnect() {
 		try {
-            Class.forName("java.sql.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
-            stmt = con.createStatement();
+			Class.forName("java.sql.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
+			stmt = con.createStatement();
 
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
 
-        }
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		new OrderHistory().setVisible(true);
 	}
