0485bffbf8b46ae7696b5386a299971e8b262d11
DipRonjon <ifty2305101641@diu.edu.bd>
2024-12-19 00:24:36 +0600

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,20 +1,4 @@
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 public class Admin extends JFrame{
 	private JLabel title;
