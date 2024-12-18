a9483efe1298a72edb9347b8deb775efc13faf02
Pushpo20 <puspita23105101511@diu.edu.bd>
2024-12-19 01:22:41 +0600

    Update PaymentHistory.java

--- src/PaymentHistory.java
+++ src/PaymentHistory.java
@@ -125,7 +125,17 @@ public class PaymentHistory extends JFrame{
 
 	}
 
-	
+	public void dbConnect() {
+		try {
+			Class.forName("java.sql.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food","root","root");
+			stmt = con.createStatement();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog(null, e, "Error!", JOptionPane.ERROR_MESSAGE);
+
+		}
+	}
 
 	public static void main(String[] args) {
 		new PaymentHistory().setVisible(true);
